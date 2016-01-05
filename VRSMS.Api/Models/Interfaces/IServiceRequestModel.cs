077a864fc3ee6df5da1e40a7a6175fffcaf3e722
craigmccauley <craig.mccauley@gmail.com>
2016-01-04 22:19:44 -0700

    Adding Ninject

---
+++ VRSMS.Api/Models/Interfaces/IServiceRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRSMS.Api.Models.Interfaces
+{
+    public interface IServiceRequestModel 
+    {
+        IEnumerable<ServiceRequestEntityPlaceholder> GetAllServiceRequests();
+
+        ServiceRequestEntityPlaceholder GetServiceRequestById(int id);
+
+        void CreateServiceRequest(ServiceRequestEntityPlaceholder value);
+
+        void UpdateServiceRequest(int id, ServiceRequestEntityPlaceholder value);
+    }
+}
---
+++ VRSMS.Api/Models/ServiceRequestEntityPlaceholder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VRSMS.Api.Models.Interfaces;
+
+namespace VRSMS.Api.Models
+{
+    public class ServiceRequestEntityPlaceholder
+    {
+    }
+}
