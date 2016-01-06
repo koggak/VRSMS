a6647c4c9ac020aafca7752eb8f5dcc8dd215c3f
craigmccauley <craig.mccauley@gmail.com>
2016-01-05 19:07:32 -0700

    Added missing file

---
+++ src/VRSMS/VRSMS.Api/Models/ServiceRequestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VRSMS.Api.Models.Interfaces;
+
+namespace VRSMS.Api.Models
+{
+    public class ServiceRequestModel : IServiceRequestModel
+    {
+        
+        public IEnumerable<ServiceRequestEntityPlaceholder> GetAllServiceRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceRequestEntityPlaceholder GetServiceRequestById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateServiceRequest(ServiceRequestEntityPlaceholder value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateServiceRequest(int id, ServiceRequestEntityPlaceholder value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
